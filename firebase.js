5c5f5f9172d492af5f325f517e61573158d4b490
Karim Hesham <karimhesham00@hotmail.com>
2021-08-19 18:34:51 +0200

    Implemented Checkout Component - CheckoutProduct Component - Integrated Redux

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCXWPPMtemF3ROp843D64YnsnPlNibVjZU",
+  authDomain: "clone2-b8ccf.firebaseapp.com",
+  projectId: "clone2-b8ccf",
+  storageBucket: "clone2-b8ccf.appspot.com",
+  messagingSenderId: "1030852754733",
+  appId: "1:1030852754733:web:253bfdfcae228f323fa7a4",
+};
